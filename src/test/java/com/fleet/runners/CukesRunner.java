467ac7ee599f22ba49e2e03c74155da15d6a569f
simer <enderkamaci@me.com>
2025-02-19 23:17:52 -0500

    still same

--- src/test/java/com/fleet/runners/CukesRunner.java
+++ src/test/java/com/fleet/runners/CukesRunner.java
@@ -13,9 +13,9 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/fleet/step_definitions",
-        dryRun = false,
+        dryRun = true,
         tags = "",
-        publish = true //generating a report with public link
+        publish = false //generating a report with public link
 )
 public class CukesRunner {}
 
