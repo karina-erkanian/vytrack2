8605fecd5c3111b080f25b5c37d8614b42c10dbf
CyberLab <Marianikk122@hotmail.com>
2025-02-27 15:54:59 -0500

    US07 Updated

--- src/test/java/com/fleet/step_definitions/US07_Select_Vehicle.java
+++ src/test/java/com/fleet/step_definitions/US07_Select_Vehicle.java
@@ -9,6 +9,7 @@ import com.fleet.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.Select;
 
 
 public class US07_Select_Vehicle {
@@ -16,23 +17,25 @@ public class US07_Select_Vehicle {
     private BasePage basePage;
     LoginPage loginPage = new LoginPage();  //instantiate object
 
+    /*
     @Given("the user is logged in as {string}")
     public void theUserIsLoggedInAs(String arg0) {
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));  //Open browser
 
-        //loginPage = new LoginPage();
-
         //Run both sales and store logins. Change in feature class
-        if("sales".equals(arg0)) {
+        if ("sales".equals(arg0)) {
             loginPage.userName.sendKeys(ConfigurationReader.getProperty("sales_manager_username"));
             loginPage.password.sendKeys(ConfigurationReader.getProperty("sales_manager_password"));
             loginPage.submit.click();
-        }else if("store".equals(arg0)) {
+        } else if ("store".equals(arg0)) {
             loginPage.userName.sendKeys(ConfigurationReader.getProperty("store_manager_username"));
             loginPage.password.sendKeys(ConfigurationReader.getProperty("store_manager_password"));
             loginPage.submit.click();
         }
+
     }
+
+     */
 
 
     private HoverOptionsUnderFleetPage hoverOptionsUnderFleetPage = new HoverOptionsUnderFleetPage();
@@ -41,46 +44,33 @@ public class US07_Select_Vehicle {
     public void userHoversOverFleetAndClicksOnVehicles() {
 
         hoverOptionsUnderFleetPage.VehiclesHoverAndClick();  //Hovering and clicking stored in pages for efficiency
-
-        // basePage = new BasePage() {};  //We do this because BasePage is abstract..
-        /*
-        Actions actions = new Actions(Driver.getDriver()); //Create instance of Actions to use interactions
-
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
-        WebElement fleetMenu = wait.until(ExpectedConditions.visibilityOf(basePage.menuOptions.get(1)));
-
-        actions.moveToElement(basePage.menuOptions.get(1)).perform(); //Hover
-
-        BrowserUtils.sleep(1);
-
-        WebElement vehicleOption = Driver.getDriver().findElement(By.xpath("//a[@href='entity/Extend_Entity_Carreservation']"));
-
-        vehicleOption.click();
-
-         */
-
     }
 
     SelectVehicleCheckBoxesPage selectVehicleCheckBoxesPage = new SelectVehicleCheckBoxesPage();
+
+    @Then("the user should be able to see checkboxes unchecked")
+    public void theUserShouldBeAbleToSeeCheckboxesUnchecked() {
+        SelectVehicleCheckBoxesPage.checkUnselectedCheckBoxes();
+    }
+
+    @Then("the user should be able to select all checkboxes")
+    public void theUserShouldBeAbleToSelectAllCheckboxes() {
+        SelectVehicleCheckBoxesPage.selectAllCheckBoxes();
+    }
 
     @Then("the user should be able select on any vehicle {int}")
     public void theUserShouldBeAbleSelectOnAnyVehicle(int index) {
 
 
-         SelectVehicleCheckBoxesPage.selectCheckBox(1);
-         SelectVehicleCheckBoxesPage.selectCheckBox(2);
-         SelectVehicleCheckBoxesPage.selectCheckBox(3);
-
-         //To select All
-        //selectVehicleCheckBoxesPage.selectCheckBox(index);
-
-
-
+        SelectVehicleCheckBoxesPage.selectCheckBox(1);
+        SelectVehicleCheckBoxesPage.selectCheckBox(2);
+        SelectVehicleCheckBoxesPage.selectCheckBox(3);
 
 
     }
+}
 
 
 
-    }
+
 
